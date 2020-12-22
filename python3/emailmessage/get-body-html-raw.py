2f7f75b419b2f4be7842d8ebe436ee83ec5a42fc
Bastian Graebener <Bastian.Graebener@ibm.com>
2020-12-22 13:28:35 +0000

    fix comment

--- python3/emailmessage/get-body-html-raw.py
+++ python3/emailmessage/get-body-html-raw.py
@@ -1,5 +1,5 @@
 # (c) Copyright IBM Corp. 2010, 2020. All Rights Reserved.
 
-# Make a call to get the raw body of an incident
+# Make a call to get the raw body of an email message
 html = emailmessage.getBodyHtmlRaw()
 log.info(html)
